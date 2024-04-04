496b355df3b9e2f96399592ebea3deefa6327740
Raddames <raddamestonui48@gmail.com>
2024-04-04 23:49:05 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // code your solution here
+const cats = ["Milo", "Otis", "Garfield"]
+const record = [
+  { year: "2015", result: "W" },
+  { year: "2014", result: "N/A" },
+  { year: "2013", result: "L" },
+  //...
+];
+
+function superbowlWin(objects){
+    const winRecord = objects.find( obj => obj.result === "W")
+    if (winRecord){
+      return winRecord.year;
+    }
+    else{
+      return undefined;
+    }
+}
+
